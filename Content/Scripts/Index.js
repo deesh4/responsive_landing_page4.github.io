37f493be992724cb49d5359bf314e98091e2d477
deesh4 <68685356+deesh4@users.noreply.github.com>
2021-03-24 18:35:29 +0530

    responsive_landing_page4

---
+++ Content/Scripts/Index.js
@@ -0,0 +1,16 @@
+﻿
+$(document).ready(function () {
+
+    // TOGGLE BUTTON OF NAV BAR
+    $('.first-button').on('click', function () {
+        $('.animated-icon1').toggleClass('open');
+    });
+    //***************************
+});
+
+///// <reference path="Index.js" />
+//        window.setTimeout(function () {
+//            window.open("https://form.jotform.com/201973418627461", "Inquiry", "width=1000,height=1000");
+
+//            //window.open('https://form.jotform.com/201973418627461','_blank');
+//        }, 7000)
